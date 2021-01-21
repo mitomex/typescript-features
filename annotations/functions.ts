bdd7145f831acd15a1853c13ea740fb83c46fb83
Yusuke Mitome <info@mitomex.jp>
2021-01-22 08:04:07 +0900

    fixed annotations function

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,6 +2,6 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
-const subtract = (a: number, b: number) => {
-  a - b; // 本当は return するつもりがミス。だけど返り値に type annotation をつけていないためエラーは出ない。
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
