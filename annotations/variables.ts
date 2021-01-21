af83f7eba74346744b6762a0a57aaeac24a78375
Yusuke Mitome <info@mitomex.jp>
2021-01-21 22:00:25 +0900

    add some annotations variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,23 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = { // y の後にセミコロンは要らない
+  x: 10,
+  y: 20
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
