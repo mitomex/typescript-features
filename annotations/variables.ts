3ac975f737cc3c3318f3824abd7533067e1ab421
Yusuke Mitome <info@mitomex.jp>
2021-01-21 22:42:08 +0900

    add a case of any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -27,3 +27,9 @@ let point: { x: number; y: number } = { // y の後にセミコロンは要ら�
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
