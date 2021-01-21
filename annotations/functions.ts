b1c417eafa04934feb023db7ce6842f51a4ddfc8
Yusuke Mitome <info@mitomex.jp>
2021-01-22 07:54:11 +0900

    add a mistake

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,7 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number) => {
+  a - b; // 本当は return するつもりがミス。だけど返り値に type annotatio をつけていないためエラーは出ない。
+};
