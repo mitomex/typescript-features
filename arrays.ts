a6c5deb7035b0d5c67f4b576b0debffdbd38c52f
Yusuke Mitome <info@mitomex.jp>
2021-01-24 22:20:24 +0900

    add some cases of annotations of arrays

--- arrays.ts
+++ arrays.ts
@@ -15,3 +15,23 @@ const carsByMake = [
 
 const carsByMakeEmpty: string[][] = [];
 // ネストした配列の Type Annotation
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);  // Number はエラーになる
+
+// Help with `map`
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+// Type は `(string | Date)[]` となる
+
+const importantDatesOne: (string|Date)[] = [new Date()];
+// 初期化時には Type はひとつだが、あとで別の Type が入る可能性がある場合は
+// Type Annotation が必要
