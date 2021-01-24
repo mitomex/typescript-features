3f854c4d9c1bf4d475be2764e48b7f0d9ddb00ee
Yusuke Mitome <info@mitomex.jp>
2021-01-24 21:25:33 +0900

    add some cases of annotations objects

--- annotations/objects.ts
+++ annotations/objects.ts
@@ -9,3 +9,12 @@ const profile = {
     this.age = age;
   }
 };
+
+// オブジェクトを分割代入で受け取ったときの Type Annotation
+const { age }: { age: number } = profile;
+
+// const { age }: number = profile とはできない。
+// なぜなら複数プロパティを受け取る場合があるから
+
+// ネストされたオブジェクトを分割代入で受け取ったときの Type Annotation
+const { coords: { lat, lng } }: { coords: { lat: number; lng: number } } = profile;
