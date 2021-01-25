dbb56fc517af328a58a45a2ba50e7777bb8178d5
Yusuke Mitome <info@mitomex.jp>
2021-01-25 12:22:13 +0900

    add inheritance

--- classes.ts
+++ classes.ts
@@ -8,6 +8,19 @@ class Vehicle {
   }
 }
 
+// クラスの継承
+// メソッドをコピペせずに使える
+class Car extends Vehicle {
+  // メソッドの上書きも可能
+  drive(): void {
+    console.log('vroom');
+  }
+}
+
 const vehicle = new Vehicle();
 vehicle.drive();
 vehicle.honk();
+
+const car = new Car();
+car.drive();
+car.honk();
